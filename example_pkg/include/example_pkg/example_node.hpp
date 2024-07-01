86a1ecf93d0a93e43dac9a8bf781eb8ca77f26c3
vulcan <jinxu.yu@directdrivetech.com>
2024-07-01 15:08:26 +0800

    style: change code format.
    Signed-off-by: vulcan <jinxu.yu@directdrivetech.com>

--- example_pkg/include/example_pkg/example_node.hpp
+++ example_pkg/include/example_pkg/example_node.hpp
@@ -25,20 +25,7 @@
 class ExampleNode : public rclcpp::Node
 {
 public:
-  explicit ExampleNode(const rclcpp::NodeOptions & options) : Node("example_node", options)
-  {
-    this->get_parameter("value1", value1_);
-    this->get_parameter("value2", value2_);
-    RCLCPP_INFO(this->get_logger(), "value1 : '%f'", value1_);
-
-    subscription1_ = this->create_subscription<std_msgs::msg::Float64>(
-      "topic1", 10, std::bind(&ExampleNode::topic1_callback, this, std::placeholders::_1));
-    subscription2_ = this->create_subscription<std_msgs::msg::Float64>(
-      "topic2", 10, std::bind(&ExampleNode::topic2_callback, this, std::placeholders::_1));
-    publisher_ = this->create_publisher<std_msgs::msg::Float64>("sum", 10);
-    timer_ = this->create_wall_timer(
-      std::chrono::seconds(1), std::bind(&ExampleNode::timer_callback, this));
-  }
+  explicit ExampleNode(const rclcpp::NodeOptions & options);
 
 private:
   /// \brief  Publish result data once per second.
